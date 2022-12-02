784143cf39e6a0f6d6593202cbc0a089214172ee
nimin <niminonline@gmail.com>
2022-12-02 15:13:15 +0530

    v2final

--- src/App.js
+++ src/App.js
@@ -4,66 +4,62 @@ import Header from "./components/Header";
 import OGItem from "./components/OGItem";
 import DoneItem from "./components/DoneItem";
 import DroppedItem from "./components/DroppedItem";
-import React, { useState,useEffect } from "react";
+import React, { useState} from "react";
 
 
 
 function App() {
+  const [task,setTask] = useState([]);
  
-  const [ogTask, setOGTask] = useState(JSON.parse(localStorage.getItem("LocalOgList"))||[]);
-  const [doneTask, setdoneTask] = useState(JSON.parse(localStorage.getItem("LocalDoneList"))||[]);
-  const [droppedTask, setDroppedTask] = useState(JSON.parse(localStorage.getItem("LocalDroppedList"))||[]);
-  const [listname,setListname]= useState("OG");
+  // const [ogTask, setOGTask] = useState(JSON.parse(localStorage.getItem("LocalOgList"))||[]);
+  // const [doneTask, setdoneTask] = useState(JSON.parse(localStorage.getItem("LocalDoneList"))||[]);
+  // const [droppedTask, setDroppedTask] = useState(JSON.parse(localStorage.getItem("LocalDroppedList"))||[]);
+  // 
 
-  useEffect(()=>{
+  // useEffect(()=>{
 
-    localStorage.setItem("LocalOgList", JSON.stringify(ogTask));
-    localStorage.setItem("LocalDoneList", JSON.stringify(doneTask));
-    localStorage.setItem("LocalDroppedList", JSON.stringify(droppedTask));
+  //   localStorage.setItem("LocalOgList", JSON.stringify(ogTask));
+  //   localStorage.setItem("LocalDoneList", JSON.stringify(doneTask));
+  //   localStorage.setItem("LocalDroppedList", JSON.stringify(droppedTask));
 
-  },[ogTask,doneTask,droppedTask])
+  // },[ogTask,doneTask,droppedTask])
 
 
-
-  
  
   function itemAddonClick(task) {
     console.log("New Item Added to Ongoing List");
-    setOGTask((prevVal) => [...prevVal,{ id: Date.now(), data: task, list: listname, date: new Date().toLocaleString("en-IN")}]);
+    setTask((prevVal) => [...prevVal,{ id: Date.now(), data: task, list: "OG", date: new Date().toLocaleString("en-IN")}]);
   }
 
-  function doneTaskFun(id){
-    setdoneTask((prevVal)=> [...prevVal, ...ogTask.filter((element)=> element.id===id) ] )
-    setOGTask((prevVal) => [...ogTask.filter((element) => element.id !== id)]);
-    console.log("Task" + id +" moved to finished list")
+  function doneTaskFun(id){ 
+    setTask([...task.filter((element)=>(element.id!==id)),...task.filter((element)=>(element.id===id)&&(element.list="done"))])
+    //task.map((element)=>(element.id===id)&& (element.list="done"));
+
+    console.log("Task" + id +" moved to finished list");
   }
 
   function deleteDoneTask(id){
     if(window.confirm("Are you sure to remove the task?")){
-      setdoneTask([...doneTask.filter((element)=> element.id!==id)])
-      console.log("Task "+ id +" has been removed");
-
-    }
+      setTask([...task.filter((element)=> element.id!==id)])
+      console.log("Task "+ id +" has been removed");}
   }
 
   function deleteOGTask(id) {
-    //let newArr= ogTask.filter((element)=> element.id===id);
-    setDroppedTask((prevVal) =>[...prevVal,...ogTask.filter((element) => element.id === id)]);
-
-    setOGTask((prevVal) => [...ogTask.filter((element) => element.id !== id)]);
-    console.log("Task " + id + " moved to dropped list");
+    setTask([...task.filter((element)=>(element.id!==id)),...task.filter((element)=>(element.id===id)&&(element.list="dropped"))])
+    task.map((element) => (element.id=== id) && (element.list="dropped"));
+   console.log("Task " + id + " moved to dropped list");
   }
 
   function deleteDroppedTask(id){
    if(window.confirm("Are you sure to remove the task?")===true)
    {
-    setDroppedTask(()=> droppedTask.filter((element)=> element.id !==id))
+    setTask(()=> task.filter((element)=> element.id !==id))
    }
   }
 
   function restoreDroppedTask(id){
-    setOGTask((prevVal)=>[...prevVal, ...droppedTask.filter((element)=>element.id === id)]);
-    setDroppedTask(()=> droppedTask.filter((element)=> element.id !==id))
+    setTask([...task.filter((element)=>(element.id!==id)),...task.filter((element)=>(element.id===id)&&(element.list="OG"))])
+    
     console.log("Task "+ id+ " restored to ongoing List");
   }
 
@@ -75,8 +71,8 @@ function App() {
       <div className="listGroupDiv row">
         <div className="listDiv DoneItem">
           <p className="listTitle">Completed Tasks</p>
-          {doneTask.map((element)=>
-          <DoneItem 
+          {(task.filter((elements)=>elements.list==="done")).map((element)=>
+            <DoneItem 
             key={element.id}
             id ={element.id}
             data={element.data}
@@ -88,7 +84,7 @@ function App() {
 
         <div className="listDiv OGItem">
           <p className="listTitle"> Ongoing Tasks</p>
-          {ogTask.map((element) => (
+          {(task.filter((elements)=>elements.list==="OG")).map((element) => (
             <OGItem
               id={element.id}
               key={element.id}
@@ -103,7 +99,7 @@ function App() {
 
         <div className="listDiv DroppedItem">
           <p className="listTitle"> Deleted Tasks</p>
-          {droppedTask.map((element) => (
+          {(task.filter((elements)=>elements.list==="dropped")).map((element) => (
             <DroppedItem
               id={element.id}
               key={element.id}
